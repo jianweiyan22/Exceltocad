9b2761d03bc1136f03e678be5cac59c25548f452
jianweiyan22 <jianweiyan22@gmail.com>
2026-09-07 10:58:42 +0800

    feat: add desktop application entry point

---
+++ src/Excel2GuanLiDe/Program.cs
@@ -0,0 +1,11 @@
+namespace Excel2GuanLiDe;
+
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
